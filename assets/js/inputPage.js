46e91bbaccafe875079fb39dd749619d03f76ffb
AdminChatter <xiangzhenwuyu@gmail.com>
2024-07-09 19:19:29 -0400

    finish prototype of the inputpage and time card logic

--- assets/js/inputPage.js
+++ assets/js/inputPage.js
@@ -1,26 +1,40 @@
 const form = document.querySelector('form');
 
-const formSubmission = () => {
+//Check the validation of the input
+const validInput = (payrate, company) => {
+    if (!payrate){
+        window.alert('Missing Payrate Input!');
+        return false;
+    }else if (!company){
+        window.alert('Missing Company Input!');
+        return false;
+    }else if (typeof payrate !== "number"){
+        window.alert('The payrate need to be an number.');
+        return false;
+    } else {
+        return true;
+    }
+}
+
+//Handles the form submission. Grab the form data and store it in local storage
+const formSubmission = (event) => {
     event.preventDefault();
 
     const payRate = document.getElementById('payRate').value;
     const userCompany = document.getElementById('company').value;
 
-    if (!payRate){
-        window.alert('missing Payrate Input!');
-        return;
-    }else if (!userCompany){
-        window.alert('missing Company Input!');
-    }else{
+    if (validInput(payRate, userCompany)) {
         const payInfo = {
             payrate,
             company,
-            workhour: 0
+            workhour: 0,
         };
-
         //store the user information in local storage
-        const existingData = localStorage.getItem('payInfo')
-        existingData.push(payInfo)
+        const existingData = JSON.parse(localStorage.getItem('payInfo'));
+        existingData.push(payInfo);
         localStorage.setItem('payInfo',JSON.stringify('payInfo'));
     }
 }
+
+//Call the function to handle the form submission
+form.addEventListener('submit', formSubmission)
