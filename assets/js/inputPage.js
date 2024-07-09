1e82d19367d76f0b6957cd754ade78f73c1bf49a
AdminChatter <xiangzhenwuyu@gmail.com>
2024-07-08 20:03:43 -0400

    added the logic for input Page

---
+++ assets/js/inputPage.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('form');
+
+const formSubmission = () => {
+    event.preventDefault();
+
+    const payRate = document.getElementById('payRate').value;
+    const userCompany = document.getElementById('company').value;
+
+    if (!payRate){
+        window.alert('missing Payrate Input!');
+        return;
+    }else if (!userCompany){
+        window.alert('missing Company Input!');
+    }else{
+        const payInfo = {
+            payrate,
+            company
+        };
+
+        //store the user information in local storage
+        const existingData = localStorage.getItem('payInfo')
+        existingData.push(payInfo)
+        localStorage.setItem('payInfo',JSON.stringify('payInfo'));
+    }
+}
